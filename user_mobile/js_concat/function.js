51b695a1a2d404b3609e39cfa71076a3dd7194d8
junsik <burning5@madisonave.co.kr>
2015-05-09 17:33:07 +0900

    모바일페이지 추가

---
+++ user_mobile/js_concat/function.js
@@ -0,0 +1,46 @@
+// Class
+
+
+// main
+
+$(function(){
+
+//toggle gnb
+(function() {
+	$('.js-tg-gnb').on('click', function(e){
+		if ($(this).hasClass('on')) {
+			$(this).removeClass('on');
+			$('#gnb').removeClass('on');
+		} else {
+			$(this).addClass('on');
+			$('#gnb').addClass('on');
+		}
+		e.preventDefault();
+	});
+})();
+
+// toggle box
+(function(){
+	var tgTitles = $('.js-tg-title'),
+		tgConts = $('.js-tg-cont');
+
+	tgTitles.on('click', function(e){
+		var currentCont = $(this).parents('.toggle-box').find('.js-tg-cont');
+		if ($(this).hasClass('on')) {
+			tgTitles.removeClass('on');
+			currentCont.removeClass('on');
+		} else {
+			tgTitles.removeClass('on');
+			tgConts.removeClass('on');
+			$(this).addClass('on');
+			currentCont.addClass('on');
+		}
+		e.preventDefault();
+	});
+
+	if (tgTitles.length == 1 && tgConts.length == 1) {
+		tgTitles.eq(0).click();
+	}
+})();
+
+});
